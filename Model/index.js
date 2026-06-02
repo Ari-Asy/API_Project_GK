95f6ab8ef6545a0e8f4f5120909992514660f4cb
Ari-Asy <ksroooppp1223@gmail.com>
2026-06-02 17:06:08 +0700

    VBata 0.4

--- Model/index.js
+++ Model/index.js
@@ -1,5 +1,9 @@
-const {Sequelize,DataTypes} = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:1234@localhost:5432/postgres',{dialect:"postgres"});
+const {Sequelize,DataTypes} = require('sequelize');//นำเข้า Sequelize และ DataTypes จากไลบรารี sequelize
+const sequelize = new Sequelize('mydatabase','myuser','mypassword',{
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres'
+});// คือการเชื่อมต่อฐานข้อมูล PostgreSQL โดยใช้ Sequelize ORM
 
 sequelize.authenticate().then(() => {
     console.log('Database เชื่อมต่อสำเร็จ')
@@ -10,3 +14,15 @@ sequelize.authenticate().then(() => {
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+
+//นำเข้าโมเดล User จากไฟล์ userModel.js และเก็บไว้ใน db.User
+db.User = require('./userModel')(sequelize,DataTypes);
+
+//ซิงค์โมเดลกับฐานข้อมูล โดยการสร้างตารางถ้ายังไม่มีอยู่
+sequelize.sync().then(() => {
+    console.log('ตารางในฐานข้อมูลถูกสร้างเรียบร้อยแล้ว');
+}).catch((err) => {
+    console.error('เกิดข้อผิดพลาดในการสร้างตาราง:', err);
+});
+
+module.exports = db;
